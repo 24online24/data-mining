d8ea3084c2bd3aba60eb6475919bb676fd5d62d8
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2024-11-26 21:31:57 +0200

    Move TSP examples

--- NeuralNetworks/exemplu_curs_1.py
+++ NeuralNetworks/exemplu_curs_1.py
@@ -1,18 +1,47 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
-from tensorflow
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
 
-# x_train, x_test = x_train / 255.0, x_test / 255.0
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# model = Sequential([
-#   Flatten(input_shape=(28, 28)),
-#   Dense(128, activation='relu'),
-#   Dense(64, activation='relu'),
-#   Dense(10, activation='softmax')
-# ])
+print(f"Forma lui x_train: {x_train.shape}")
+print(f"Forma lui y_train: {y_train.shape}")
 
-# history = model.fit(x_train, y_train, epochs = 10, batch_size=32, validation_split=0.1)
+plt.figure(figsize=(10, 10))
+for i in range(9):
+    plt.subplot(3, 3, i + 1)
+    plt.imshow(x_train[i], cmap='gray')
+    plt.title(f"Label: {y_train[i]}")
+    plt.axis('off')
+plt.show()
 
-# test_lost, test_mae = model.evaluate
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+y_train = to_categorical(y_train, 10)
+
+y_test_cat = to_categorical(y_test, 10)
+
+model = Sequential([
+  Flatten(input_shape=(28, 28)),
+  Dense(128, activation='relu'),
+  Dense(64, activation='relu'),
+  Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', 'Precision', 'Recall'])
+
+history = model.fit(x_train, y_train, epochs = 10, batch_size=32, validation_split=0.1)
+
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(loc="lower right")
+plt.show()
